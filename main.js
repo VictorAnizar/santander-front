eabb0e99031cb29168c8af993047990a75146951
VictorAnizar <victoranizarmorales@gmail.com>
2021-07-07 19:32:32 -0500

    agregando sass

---
+++ main.js
@@ -0,0 +1,5 @@
+//Crear proyecto de node
+//npm init -> Agrega la config
+//npm i sass -> instalar la dependencia de sass desde https://www.npmjs.com/package/sass
+//crear .gitignore t agregar tecto  de la pag https://www.toptal.com/developers/gitignore/api/node
+//correr sass: sass --watch archivo.scss salida.css
