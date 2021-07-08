268af53d43290180e6f6564196e4d3bc8b1b1189
VictorAnizar <victoranizarmorales@gmail.com>
2021-07-07 21:37:14 -0500

    Configuracion email

--- main.js
+++ main.js
@@ -1,30 +1,92 @@
-'use strict'
-//Crear proyecto de node
-//npm init -> Agrega la config
-//npm i sass -> instalar la dependencia de sass desde https://www.npmjs.com/package/sass
-//crear .gitignore t agregar tecto  de la pag https://www.toptal.com/developers/gitignore/api/node
-//correr sass: sass --watch archivo.scss salida.css
 
-const form=document.querySelectorAll(".signup-form");
+// Crear proyecto de node
 
-const getTemplate = function(){
-    return fetch("./template.html").then(response=>response.text());
+// npm init -> agregar la configuración
+
+// npm i sass -> instalar dependencia de sass desde https://www.npmjs.com/package/sass
+
+// crear .gitignore y agregar texto de la pagina -> https://www.toptal.com/developers/gitignore/api/node
+
+// ejecutar comando -> ./node_modules/sass/sass.js --watch ./scss/main.scss output.css
+
+// Si es desde windows -> node ./node_modules/sass/sass.js --watch ./scss/main.scss output.css
+
+
+// Variables -----------------------------------------------------------
+
+// const x = 5
+// let y = 5
+
+// console.log(y);
+
+// y = 6
+
+// var z = 6
+// z = 8
+
+// console.log(z);
+// ------------------------------------------------------------
+
+
+
+const forms = document.querySelectorAll(".signup-form")
+
+// console.log(forms);
+
+// function suma(a, b) {
+//   return a + b
+// }
+
+const getTemplate = () => {
+  return fetch("template.html")
+    .then((response) => response.text())
+}
+
+const sendEmailToApi = (address, template) => {
+  fetch(`https://bedu-email-sender-api.herokuapp.com/send?id`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      address: address,
+      template: template,
+    }),
+  })
+    .then((results) => {
+      console.log(results.status);
+      if(results.status == 200){
+        alert("E-mail send!!!")
+      } else {
+        alert("Send failed")
+      }
+      document.getElementById("email").value = ""
+    })
+    .catch((error) => {
+      console.error(error);
+      document.getElementById("email").value = ""
+      alert("Send failed")
+    });
 };
 
-const sendEmail=(miVariable)=>{
-    miVariable.preventDefault();
-    const email = miVariable.target.querySelector("input").value;
-    getTemplate().then( (response)=>{
-        console.log(response);
-    } )
-    .catch( (error)=>{
-        console.log(error, "Error al obtener el template");
-    } )
-    console.log(email);
-};
+function sendEmail(miVariable) {
+  miVariable.preventDefault()
+  const email = miVariable.target.querySelector("input").value
+  getTemplate()
+    .then((template) => {
+      sendEmailToApi(email, template)
+    })
+    .catch((error) => {
+      console.log(error, "error al obtener el template");
+    })
+}
 
+// const sendEmail = (miVariable) => {
+//   miVariable.preventDefault()
+//   console.log(miVariable);
+// }
 
-for (let i = 0; i < form.length; i++) {
-    // console.log(form[i]);
-    form[i].addEventListener("submit", sendEmail)
+for(let i = 0; i < forms.length; i++){
+  // console.log(forms[i]);
+  forms[i].addEventListener("submit", sendEmail)
 }
